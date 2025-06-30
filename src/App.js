674f7ce477943a314b20a4da0f1298e2554d10d8
honker233 <1078073118@qq.com>
2025-06-30 22:41:30 +0800

    Update App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,63 @@
 //项目的根组件
 
 // App -> index.js -> public/index.html (root)
+const list = [
+  { id: 1, name: "jack" },
+  { id: 2, name: "rose" },
+  { id: 3, name: "tom" },
+];
+
+const isLogin = false;
+
+//既要传递自定义参数，还要传递事件对象e
+const handleClick = (name, e) => {
+  console.log("button被点击了", name, e);
+};
+
+const articleType = 2;
+
+function getArticleTem (type){
+  if(articleType===0){
+    return <div>我是无图文章</div>;
+  }else if(type===1){
+    return <div>我是单图模式</div>;
+  }else{
+    return <div>我是三图模式</div>;
+  }
+}
+
 function App() {
   //基础写法
   // const handleClick=()=>{
-  //   console.log('button被点击了'); 
+  //   console.log('button被点击了');
   // }
 
   //事件参数e
   //  const handleClick=(e)=>{
-  //   console.log('button被点击了',e); 
+  //   console.log('button被点击了',e);
   // }
 
   //传递自定义参数
   //  const handleClick=(name)=>{
-  //   console.log('button被点击了',name); 
+  //   console.log('button被点击了',name);
   // }
-
-  //既要传递自定义参数，还要传递事件对象e
-   const handleClick=(name,e)=>{
-    console.log('button被点击了',name,e); 
-  }
+  
   return (
     <div className="App">
-    <button onClick={(e)=>handleClick('jack',e)}>click me</button>
+
+      {getArticleTem(articleType)}
+      {/* key用来提升性能 */}
+      <ul>
+        {list.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+
+      {isLogin && <span>this is span</span>}
+      {/* 三目运算符 */}
+      {isLogin ? <span>this is span</span> : <span>loading...</span>}
+
+      <button onClick={(e) => handleClick("jack", e)}>click me</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 // 项目的入口
 // React 必要的两个核心包
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // 导入项目的根组件
-import App from './App';
+import App from "./App";
 
 //把App根组件渲染到id为root的dom节点上
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-
-    <App />
-
-);
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
